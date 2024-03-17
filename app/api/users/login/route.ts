fc5eae67bc61086aaf08dc535bb043545658df3e
ankitbourasi24 <ankitbourasi0@gmail.com>
2024-03-17 21:45:15 +0530

    login api & view done...

--- app/api/users/login/route.ts
+++ app/api/users/login/route.ts
@@ -1,1 +1,41 @@
-exportwekjfnewkjfnekjnfkjefkjefldkmckasmckmacvkC
+import { connect } from "@/app/dbConfig/dbConfig";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs"
+import User from "@/app/models/userModel";
+import jwt from "jsonwebtoken"
+connect();
+export async function POST(req:NextRequest){
+    try {
+        const requestBody = await req.json()
+        const {username,password} = requestBody
+        const user = await User.findOne({username}) 
+        console.log(username,password)
+        if(!user){
+            return NextResponse.json({message:"User does not exist!!!",status:400})
+        }
+        //check user password and hash password 
+          const validPassword =   await bcryptjs.compare(password,user.password)
+        // if both are equal then create token 
+            if(!validPassword){
+            return NextResponse.json({message:"Username or password is wrong!!!",status:400})
+                
+            }
+
+            //create token data
+            const tokenData = {
+                id:user._id,
+                username:user.username,
+                email:user.email
+            }
+            //create tokendata with secret token and with expiry
+            const token = await jwt.sign(tokenData,process.env.MY_SECRET_TOKEN!,{expiresIn:"1h"});
+            const response = NextResponse.json({message:"Login Successful",success:true})
+            //set token in the cookies so that user cant manipulate it,
+            response.cookies.set("token",token,{httpOnly:true});
+            return response;
+
+          } catch (error) {
+        return NextResponse.json({message:"Login Unsuccessful, Check your request"})
+        
+    }
+}
--- app/pages/login/page.tsx
+++ app/pages/login/page.tsx
@@ -1,20 +1,41 @@
+"use client"
+import axios from "axios"
+import { useRouter } from "next/navigation"
+import react,{ useState } from "react"
 
-export default function LoginPage() {
+function LoginPage() {
+    const [user, setUser] = useState({
+        username:"",
+        password:""
+    })
+    const router = useRouter()
+    const onLogin = async ()=>{
+      
+        try {
+
+           const response = await axios.post("/api/users/login",user)
+            console.log(response.data);
+                         router.push("/pages/signup")
+                    } catch (error: any) {
+                        console.log("LOGIN FAILED ")
+                    }
+       
+    }
     return (
         <div>
 
-            <form action="./api/data" method="post">
+            <div >
 
-                <label htmlFor="name">login Name </label>
-                <input type="text" name="name" id="name" />
+                <label htmlFor="username">Username or Email </label>
+                <input type="text" value={user.username} onChange={(e)=> setUser({...user,username:e.target.value})} name="username" id="username" />
 
+                <label htmlFor="password">Password </label>
+                <input type="password" value={user.password} onChange={(e)=> setUser({...user,password:e.target.value})} name="password" id="password" />
 
-                <label htmlFor="age"> Enter Age </label>
-                <input type="text" name="age" id="age" />
-
-                <input type="submit" value="submit" />
-
-            </form>
+                <button type="submit" onClick={onLogin}  >SUbmit        </button>
+            </div>
         </div>
-        )
+        );
 }
+
+export default LoginPage
